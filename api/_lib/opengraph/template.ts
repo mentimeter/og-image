c92db1f63295cbbbbe7022ae53534462fde9a787
Jacob Klapwijk <jacob.klapwijk@mentimeter.com>
2021-04-21 16:48:20 +0200

    right size

--- api/_lib/opengraph/template.ts
+++ api/_lib/opengraph/template.ts
@@ -122,7 +122,7 @@ export function getHtml(parsedReq: ParsedRequest) {
         <div>
             <div class="spacer">
             <div class="logo-wrapper">
-            <svg viewBox="0 0 803.87 676.39" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" role="img" height="100%" width="100%" class="r-svg"><title>Mentimeter Logo</title><g><g fill="#000000"><polygon fill="#FFB9D0" points="67.06 663 532.44 663 201.01 0 67.06 0 67.06 663"></polygon><polygon fill="#196CFF" points="602.86 0 602.86 133.95 468.91 133.95 468.91 267.9 334.96 267.9 334.96 663 736.81 663 736.81 0 602.86 0"></polygon><path fill="#FF80AB" d="M67.06,267.9V663H468.85C465.23,444.16,286.73,267.9,67.06,267.9Z"></path><path fill="#FF403D" d="M67.06,468.82V662.9h200.8C264.25,555.11,175.74,468.82,67.06,468.82Z"></path><rect y="662.97" width="803.87" height="13.41"></rect></g></g></svg>
+            <svg viewBox="0 0 803.87 676.39" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" role="img" height="255" width="255" class="r-svg"><title>Mentimeter Logo</title><g><g fill="#000000"><polygon fill="#FFB9D0" points="67.06 663 532.44 663 201.01 0 67.06 0 67.06 663"></polygon><polygon fill="#196CFF" points="602.86 0 602.86 133.95 468.91 133.95 468.91 267.9 334.96 267.9 334.96 663 736.81 663 736.81 0 602.86 0"></polygon><path fill="#FF80AB" d="M67.06,267.9V663H468.85C465.23,444.16,286.73,267.9,67.06,267.9Z"></path><path fill="#FF403D" d="M67.06,468.82V662.9h200.8C264.25,555.11,175.74,468.82,67.06,468.82Z"></path><rect y="662.97" width="803.87" height="13.41"></rect></g></g></svg>
 
             </div>
             <div class="spacer">
